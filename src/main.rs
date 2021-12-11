8b149624230f6a05ffc371d5103fcd3cb6474ddd
Jay/Fienna Liang <roackb2@gmail.com>
2021-12-11 16:12:22 +0800

    Allow user to decide whether to play the game

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,25 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
+    some_random_code();
+
+    let mut input = String::new();
+
+    println!("Should we play the guessing game? \
+        type 'play' to play. \
+    ");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Fail to parse input");
+
+    match input.trim() {
+        "play" => guessing_game(),
+        _ => println!("Alright, don't play")
+    };
+}
+
+fn guessing_game() {
     println!("Guess the number!");
 
     let secret_num = rand::thread_rng().gen_range(1..101);
@@ -35,4 +54,11 @@ fn main() {
             }
         }
     }
+}
+
+fn some_random_code() {
+    let mut some_num = 5;
+    println!("Some number: {}", some_num);
+    some_num = 6;
+    println!("Some number is changed to {}", some_num);
 }
