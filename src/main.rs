b0b72d3655835cb6e51ed78fd3d7bcae8a5a5608
Jay/Fienna Liang <roackb2@gmail.com>
2021-12-12 15:43:56 +0800

    Add code that play around compound types

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    play_around_numbers();
+    play_around_primitives();
+    play_around_compound_types();
 
     let mut input = String::new();
 
@@ -56,7 +57,7 @@ fn guessing_game() {
     }
 }
 
-fn play_around_numbers() {
+fn play_around_primitives() {
     let mut some_num = 5;
     println!("Some number: {}", some_num);
     some_num = 6;
@@ -73,4 +74,21 @@ fn play_around_numbers() {
     println!("Octal representation of 0o32: {}", some_num);
     let some_num = 0b1001_0100;
     println!("Binary representation of 0b1001_0100: {}", some_num);
+    let some_num: f32 = 28.97;
+    println!("A floating point number: {}", some_num);
+    let txt = "😎";
+    println!("A char could be emoji: {}", txt);
+    let txt = "我是中文";
+    println!("Chinese is also fine: {}", txt);
+}
+
+fn play_around_compound_types () {
+    let tup: (u32, i64, bool) = (28, -12, false);
+    println!("A tuple with type (u32, char, bool): {}, {}, {}", tup.0, tup.1, tup.2);
+    let (a, b, c) = tup;
+    println!("Tuple elements could also be destructured: {}, {}, {}", a, b, c);
+    let arr: [u32; 3] = [2, 5, 8];
+    println!("Array is of fixed length: [{}, {}, {}]", arr[0], arr[1], arr[2]);
+    let arr = [5; 3];
+    println!("Array initialization: [{}, {}, {}]", arr[0], arr[1], arr[2]);
 }
