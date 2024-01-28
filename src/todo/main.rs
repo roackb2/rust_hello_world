3c9eac6bcbd50e36c5077ab1fe9c5816129c0013
Jay/Fienna Liang <roackb2@gmail.com>
2024-01-28 15:33:58 +0800

    Use Cli structure for command line args

--- src/todo/main.rs
+++ src/todo/main.rs
@@ -1,13 +1,31 @@
 
-use std::{io, string};
+use core::fmt;
+use std::{error, io, string};
 
-fn main () {
-  print_prompt();
+#[derive(Debug)]
+enum Command {
+  Add,
+  Amend,
+  Delete,
+  List,
+  Unknown
+}
 
-  let cmd = parse_arg(1, "Command is required");
-  let content = parse_arg(2, "Item content is required");
+impl fmt::Display for Command {
+  fn fmt (&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{:?}", self)
+  }
+}
 
-  println!("Command: {}, content: {}", cmd, content);
+struct Cli {
+  command: Command,
+  content: String
+}
+
+impl Cli {
+  fn print (&self) {
+    println!("Command: {}, content: {}", self.command, self.content);
+  }
 }
 
 fn print_prompt () {
@@ -17,4 +35,27 @@ fn print_prompt () {
 fn parse_arg (pos: usize, msg: &str) -> String {
   let mut args = std::env::args();
   return args.nth(pos).expect(msg);
+}
+
+fn parse_args () -> Cli {
+  let cmd = parse_arg(1, "Command is required");
+  let content = parse_arg(2, "Item content is required");
+
+  let cli = match cmd.as_str() {
+    "add" => Cli { command: Command::Add, content: content },
+    "amend" => Cli { command: Command::Amend, content: content },
+    "delete" => Cli { command: Command::Delete, content: content },
+    "list" => Cli { command: Command::List, content: content },
+    _ => Cli { command: Command::Unknown, content: content }
+  };
+
+  return cli;
+}
+
+fn main () {
+  print_prompt();
+
+  let cli = parse_args();
+
+  cli.print();
 }
