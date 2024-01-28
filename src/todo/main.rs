9f7c4678774ba46a24962f29e020f9a30658462b
Jay/Fienna Liang <roackb2@gmail.com>
2024-01-28 14:52:07 +0800

    Naive parsing command line arguments

--- src/todo/main.rs
+++ src/todo/main.rs
@@ -1,6 +1,20 @@
 
-use std::io;
+use std::{io, string};
 
 fn main () {
-  println!("A command line TODO app")
+  print_prompt();
+
+  let cmd = parse_arg(1, "Command is required");
+  let content = parse_arg(2, "Item content is required");
+
+  println!("Command: {}, content: {}", cmd, content);
+}
+
+fn print_prompt () {
+  println!("A command line TODO app.");
+}
+
+fn parse_arg (pos: usize, msg: &str) -> String {
+  let mut args = std::env::args();
+  return args.nth(pos).expect(msg);
 }
